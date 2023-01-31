f64fa26a5d6e8a88ada86bc358a3fad66ee3606b
Dominik Kotecki <Dominik.Kotecki@rossmann.com.pl>
2023-01-31 16:21:26 +0100

    password

--- src/TestContainers.Minio/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace TestContainers.Minio
-{
-    public class Class1
-    {
-    }
-}
---
+++ src/TestContainers.Minio/Configuration/MinioTestcontainerConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using DotNet.Testcontainers.Configurations;
+
+namespace TestContainers.Minio.Configuration;
+
+public sealed class MinioTestcontainerConfiguration : TestcontainerDatabaseConfiguration
+{
+    private const string MinioImage = "quay.io/minio/minio";
+    private const int MinioPort = 9000;
+    private const string MinioUser = "ROOTNAME";
+    private const string MinioPassword = "CHANGEME123";
+
+    public MinioTestcontainerConfiguration(string image = MinioImage) : base(image, MinioPort)
+    {
+        this.Environments.Add("MINIO_ROOT_USER", Username);
+        this.Environments.Add("MINIO_ROOT_PASSWORD", Password);
+    }
+    
+    public override string Database
+    {
+        get => string.Empty;
+        set => throw new NotImplementedException();
+    }
+
+    /// <inheritdoc />
+    public override string Username { get; set; } = MinioUser;
+
+    public override string Password { get; set; } = MinioPassword;
+}
