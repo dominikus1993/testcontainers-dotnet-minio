3df2105932a0397eff8ae16241c5474d998565e3
Dominik.Kotecki <dominikus1910@gmail.com>
2023-02-01 18:37:17 +0100

    add sime module config

---
+++ src/TestContainers.Minio/Configuration/MinioConfiguration.cs
@@ -0,0 +1,61 @@
+using Docker.DotNet.Models;
+using DotNet.Testcontainers.Configurations;
+using JetBrains.Annotations;
+
+namespace TestContainers.Minio.Configuration;
+
+[PublicAPI]
+public sealed class MinioConfiguration : ContainerConfiguration
+{
+        /// <summary>
+    /// Initializes a new instance of the <see cref="ModuleNameConfiguration" /> class.
+    /// </summary>
+    /// <param name="config">The ModuleName config.</param>
+    public MinioConfiguration(object? config = null)
+    {
+        // // Sets the custom builder methods property values.
+        // Config = config;
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ModuleNameConfiguration" /> class.
+    /// </summary>
+    /// <param name="resourceConfiguration">The Docker resource configuration.</param>
+    public MinioConfiguration(IResourceConfiguration<CreateContainerParameters> resourceConfiguration)
+        : base(resourceConfiguration)
+    {
+        // Passes the configuration upwards to the base implementations to create an updated immutable copy.
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ModuleNameConfiguration" /> class.
+    /// </summary>
+    /// <param name="resourceConfiguration">The Docker resource configuration.</param>
+    public MinioConfiguration(IContainerConfiguration resourceConfiguration)
+        : base(resourceConfiguration)
+    {
+        // Passes the configuration upwards to the base implementations to create an updated immutable copy.
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ModuleNameConfiguration" /> class.
+    /// </summary>
+    /// <param name="resourceConfiguration">The Docker resource configuration.</param>
+    public MinioConfiguration(MinioConfiguration resourceConfiguration)
+        : this(new MinioConfiguration(), resourceConfiguration)
+    {
+        // Passes the configuration upwards to the base implementations to create an updated immutable copy.
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ModuleNameConfiguration" /> class.
+    /// </summary>
+    /// <param name="oldValue">The old Docker resource configuration.</param>
+    /// <param name="newValue">The new Docker resource configuration.</param>
+    public MinioConfiguration(MinioConfiguration oldValue, MinioConfiguration newValue)
+        : base(oldValue, newValue)
+    {
+        // // Create an updated immutable copy of the module configuration.
+        // Config = BuildConfiguration.Combine(oldValue.Config, newValue.Config);
+    }
+}
